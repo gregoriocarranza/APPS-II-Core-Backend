35043f316e44b41623471618a30bde0b8315af0c
gregoriocarranza <gregoriocarranza@hotmail.com>
2025-10-29 19:59:02 -0300

    cors allow all

--- src/common/config/origins/origins.config.ts
+++ src/common/config/origins/origins.config.ts
@@ -1,5 +1,8 @@
 export const getAllowedOrigins = (): string[] => {
   let origins: string = process.env.CORS_ALLOWED_ORIGINS || "localhost";
+  if (!origin) {
+    return ["*"]
+  }
   origins = origins
     .split("\n")
     .join("")
