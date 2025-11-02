ed6b47a7f80d9bf62472613f8a27883303b773b5
Gregorio Carranza <gregoriocarranza@hotmail.com>
2025-11-02 20:56:15 -0300

    ecosistem

---
+++ ecosystem.config.js
@@ -0,0 +1,18 @@
+require('dotenv').config()
+
+
+module.exports = {
+  apps: [
+    {
+      name: "core-backend",
+      script: "dist/server.js",
+      cwd: "/home/ubuntu/APPS-II-Core-Backend",
+      out_file: "/home/ubuntu/logs/core-backend.log",
+      error_file: "/home/ubuntu/logs/core-backend.log",
+      log_date_format: "YYYY-MM-DD HH:mm:ss",
+      env: {
+        ...process.env
+      }
+    }
+  ]
+};
